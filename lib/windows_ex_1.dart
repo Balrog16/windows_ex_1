e9a9ade1e2728f2dfe956bb9d0e8673660875d6b
Bhamidipati <suryanarayana.bhamidipati@siemens.com>
2020-11-24 16:19:23 +0530

    add getBatteryStatus

--- lib/windows_ex_1.dart
+++ lib/windows_ex_1.dart
@@ -1,14 +1,17 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class WindowsEx_1 {
-  static const MethodChannel _channel =
-      const MethodChannel('windows_ex_1');
+  static const MethodChannel _channel = const MethodChannel('windows_ex_1');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> get batteryStatus async {
+    final String strStatus = await _channel.invokeMethod('getBatteryStatus');
+    return strStatus;
   }
 }
